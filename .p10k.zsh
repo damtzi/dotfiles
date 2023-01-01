dbe3d3e1bb24a2ec0d0116980520435a7408a375
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2023-01-01 14:37:34 +0100

    chore: Update p10k config

--- .p10k.zsh
+++ .p10k.zsh
@@ -96,8 +96,8 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    wifi                  # wifi speed
-    time                    # current time
+    # wifi                  # wifi speed
+    # time                    # current time
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
